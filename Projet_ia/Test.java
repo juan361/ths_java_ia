2f2a0dffc0a363e8affddf9d2882749801a66c8d
Xourel <mathias.audibert@isen.yncrea.fr>
2023-06-06 23:19:23 +0200

    Ia a lair de marher

--- Projet_ia/Test.java
+++ Projet_ia/Test.java
@@ -1,42 +1,41 @@
 public class Test {
     public static void main(String[] args) {
+        //Obtention du fichier audio
         Son son = new Son(
                 "C:/Users/matau/OneDrive/Documents/Etude/prepa isen/3eme_annee/Projet_IA/dataset_projet_Java_IA_THS_2023/dataset_projet_Java_IA_THS_2023/oiseaux/Chouette_Effraie.wav");
-        int[] tableentry = { 55125, 33075, 121275, 165375 };
-        float[] tableresult = { 1, 1, 0, 0 };
-        float[] donnees = son.donnees();
-        int tailleBloc = 4096; // Taille de votre bloc
+        //Matrice pour les neurones
+        int[] tableentry = { 18, 54, 35 };
+        float[] tableresult = { 1, 1, 0};
+        int tailleBloc = 4096/2; // Taille de votre bloc
         float tablmodule[][] = new float[tableresult.length][tailleBloc];
 
         for (int k = 0; k < tableresult.length; k++) {
             // Obtenez les données audio et appliquez la FFT
-
-            // int nombreBlocs = donnees.length / tailleBloc;
-            Complexe/* [] */[] fftResultat = new Complexe/* [nombreBlocs] */[tailleBloc];
             // Créer un tableau de complexe pour le bloc actuel
-            // System.out.println(son.donnees().length);
             float[] donneesBloc = son.bloc_deTaille(tableentry[k], tailleBloc);
             Complexe[] cplx = new Complexe[tailleBloc];
             for (int i = 0; i < tailleBloc; i++) {
                 cplx[i] = new ComplexeCartesien(donneesBloc[i], 0);
             }
             // Appliquer la FFT
+            Complexe/* [] */[] fftResultat = new Complexe/* [nombreBlocs] */[tailleBloc];
             fftResultat = FFTCplx.appliqueSur(cplx);
             double/* [] */[] result = new double/* [nombreBlocs] */[tailleBloc];
 
-            /*
-             * for (int i=0;i<nombreBlocs;i++)
-             * {
-             */
             for (int j = 0; j < tailleBloc; j++) {
                 result/* [i] */[j] = fftResultat/* [i] */[j].mod();
-                System.out.println(result/* [i] */[j]);
+                //System.out.println(result/* [i] */[j]);
                 tablmodule[k][j] = (float) result[j];
             }
-            final iNeurone n = new NeuroneHeavyside(tablmodule[0].length);
+            
+            // }
+        }
+        //
+        final iNeurone n = new NeuroneHeavyside(tablmodule[0].length);
+            System.out.println("Apprentissage…");
             System.out.println("Nombre de tours : " + n.apprentissage(tablmodule, tableresult));
             final Neurone vueNeurone = (Neurone) n;
-            System.out.print("Synapses : ");
+            //System.out.print("Synapses : ");
            // for (final float f : vueNeurone.synapses())
                // System.out.print(f + " ");
             //System.out.print("\nBiais : ");
@@ -50,8 +49,5 @@ public class Test {
                 // On affiche cette sortie
                 System.out.println("Entree " + i + " : " + n.sortie());
             }
-            // }
-        }
-        //
     }
 }
